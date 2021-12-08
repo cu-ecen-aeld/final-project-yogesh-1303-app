d032c4624ae36851f44078b11bcfa6722584c0a2
yogesh-1303 <yogesh.tripathi@colorado.edu>
2021-12-08 13:57:32 -0700

    cleaned

--- test_hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-	printf("Hello! This is just a test for cross compilation, if this prints, the cross compilation is successfull\n");
-	puts("this is a string");
-	return 0;
-}
